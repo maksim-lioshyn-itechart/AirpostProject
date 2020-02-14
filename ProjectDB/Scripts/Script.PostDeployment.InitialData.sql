dedaea311b01c1a368371b69785803d5a107dfbd
Maksim Lioshyn <maksim.lioshyn@itechart-group.com>
2020-02-14 16:17:03 +0300

    Update postdeployment script

--- ProjectDB/Scripts/Script.PostDeployment.InitialData.sql
+++ ProjectDB/Scripts/Script.PostDeployment.InitialData.sql
@@ -9,23 +9,67 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]                    
 --------------------------------------------------------------------------------------
 */
+Declare  @AirplaneTypeCommercialAirplaneId uniqueidentifier;
+Declare  @AirplaneTypePrivateJetId uniqueidentifier;
+Declare  @AirplaneTypePrivatePropellorPlaneId uniqueidentifier;
+Set @AirplaneTypeCommercialAirplaneId = CAST(newid() AS uniqueidentifier)
+Set @AirplaneTypePrivateJetId = CAST(newid() AS uniqueidentifier)
+Set @AirplaneTypePrivatePropellorPlaneId = CAST(newid() AS uniqueidentifier)
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'Roles')
-BEGIN
-    INSERT INTO [dbo].[Roles] ([Name]) VALUES ('ADMIN')
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'UserRoles')
+    IF (SELECT COUNT([dbo].[UserRoles].[Name]) FROM [dbo].[UserRoles]) = 0
+        BEGIN
+            INSERT INTO [dbo].[UserRoles] ([Name]) VALUES ('ADMIN')
+            INSERT INTO [dbo].[UserRoles] ([Name]) VALUES ('AUTHORIZEDUSER')
+        END
 
-    INSERT INTO [dbo].[Roles] ([Name]) VALUES ('AUTHORIZEDUSER')
-END
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'Countries')
+    IF (SELECT COUNT([dbo].[Countries].[Name]) FROM [dbo].[Countries]) = 0
+        BEGIN
+            INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('BARBADOS', 'BB')
+            INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('BOSNIA AND HERZEGOVINA', 'BA')
+            INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('FRENCH SOUTHERN TERRITORIES', 'TF')
+            INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('GERMANY', 'DE')
+            INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('HEARD ISLAND AND MCDONALD ISLANDS', 'HM')
+        END
 
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'Countries')
-BEGIN
-INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('BARBADOS', 'BB')
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'AirplaneTypes')
+    IF (SELECT COUNT([dbo].[AirplaneTypes].[Name]) FROM [dbo].[AirplaneTypes]) = 0
+        BEGIN
+            INSERT INTO [dbo].[AirplaneTypes] ([Id], [Name]) VALUES (@AirplaneTypeCommercialAirplaneId, 'CommercialAirplanes')
+            INSERT INTO [dbo].[AirplaneTypes] ([Id], [Name]) VALUES (@AirplaneTypePrivateJetId, 'PrivateJets')
+            INSERT INTO [dbo].[AirplaneTypes] ([Id], [Name]) VALUES (@AirplaneTypePrivatePropellorPlaneId, 'PrivatePropellorPlanes')
+        END
 
-INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('BOSNIA AND HERZEGOVINA', 'BA')
 
-INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('FRENCH SOUTHERN TERRITORIES', 'TF')
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'AirplaneSubTypes')
+    IF (SELECT COUNT([dbo].[AirplaneSubTypes].[Name]) FROM [dbo].[AirplaneSubTypes]) = 0
+        BEGIN
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Jumbo Passenger Jets', @AirplaneTypeCommercialAirplaneId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('MidSize Passenger Jets', @AirplaneTypeCommercialAirplaneId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Light Passenger Jets', @AirplaneTypeCommercialAirplaneId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Passenger Turbo Props', @AirplaneTypeCommercialAirplaneId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Cargo Airplanes', @AirplaneTypeCommercialAirplaneId)
 
-INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('GERMANY', 'DE')
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('VLJ', @AirplaneTypePrivateJetId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Light Business Jets', @AirplaneTypePrivateJetId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Midsize Business Jets', @AirplaneTypePrivateJetId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Heavy Business Jets', @AirplaneTypePrivateJetId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Military Jets', @AirplaneTypePrivateJetId)
 
-INSERT INTO [dbo].[Countries] ([Name], [Code]) VALUES ('HEARD ISLAND AND MCDONALD ISLANDS', 'HM')
-END
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Private Single Engine', @AirplaneTypePrivatePropellorPlaneId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Twin Turboprops', @AirplaneTypePrivatePropellorPlaneId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Aerobatic', @AirplaneTypePrivatePropellorPlaneId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Amphibious', @AirplaneTypePrivatePropellorPlaneId)
+            INSERT INTO [dbo].[AirplaneSubTypes] ([Name], [AirplaneTypeId]) VALUES ('Military Turboprops', @AirplaneTypePrivatePropellorPlaneId)
+        END
+
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = N'ClassTypes')
+    IF (SELECT COUNT([dbo].[ClassTypes].[Name]) FROM [dbo].[ClassTypes]) = 0
+        BEGIN
+            INSERT INTO [dbo].[ClassTypes] ([Name]) VALUES ('First Class')
+            INSERT INTO [dbo].[ClassTypes] ([Name]) VALUES ('Business Class')
+            INSERT INTO [dbo].[ClassTypes] ([Name]) VALUES ('Economy Class')
+            INSERT INTO [dbo].[ClassTypes] ([Name]) VALUES ('Other')
+        END
