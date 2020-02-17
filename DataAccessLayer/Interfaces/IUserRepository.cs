2984c9c00dc064ceb05f59f60cc7de980bf6e5fe
Maksim Lioshyn <maksim.lioshyn@itechart-group.com>
2020-02-17 16:04:31 +0300

    Refactoring and fix code

--- DataAccessLayer/Interfaces/IUserRepository.cs
+++ DataAccessLayer/Interfaces/IUserRepository.cs
@@ -1,11 +1,8 @@
-﻿using System.Threading.Tasks;
-using DataAccessLayer.Models;
+﻿using DataAccessLayer.Models;
 
 namespace DataAccessLayer.Interfaces
 {
     public interface IUserRepository: IBaseRepository<User>
     {
-        User GetByName(string name);
-        Task<User> GetByNameAsync(string name);
     }
 }
