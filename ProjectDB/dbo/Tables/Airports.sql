387719734697f171fb0d6deb9560b2b5bf07b11b
Maksim Lioshyn <maksim.lioshyn@itechart-group.com>
2020-02-20 14:48:44 +0300

    Fix issues

--- ProjectDB/dbo/Tables/Airports.sql
+++ ProjectDB/dbo/Tables/Airports.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Airports] (
     [Id] UNIQUEIDENTIFIER CONSTRAINT [DF_Airports_Id] DEFAULT (newid()) NOT NULL,
-    [CountryId] UNIQUEIDENTIFIER NULL,
+    [CountryId] UNIQUEIDENTIFIER NOT NULL,
     [Name] NVARCHAR (300)   NOT NULL,
     [IsActive] BIT CONSTRAINT [DF_Airports_IsActive] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_Airports] PRIMARY KEY CLUSTERED ([Id] ASC),
--- ProjectDB/dbo/Tables/UserPasswords.sql
+++ ProjectDB/dbo/Tables/UserPasswords.sql
@@ -2,8 +2,8 @@
 (
     [Id] UNIQUEIDENTIFIER CONSTRAINT [DF_UserPasswords_Id] DEFAULT (newid()) NOT NULL,
     [UserId] UNIQUEIDENTIFIER NOT NULL, 
-    [Salt] NVARCHAR (10) NOT NULL,
-    [Hash] NVARCHAR (55) NOT NULL,
+    [Salt] BINARY(16) NOT NULL,
+    [Hash] NVARCHAR(32) NOT NULL,
     CONSTRAINT [PK_UserPasswords] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_UserPasswords_ Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
 )
