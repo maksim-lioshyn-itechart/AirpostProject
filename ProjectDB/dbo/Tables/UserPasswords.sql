0cde82d12fc6fe4eb7452fbc7b9525aa2d5685ae
Maksim Lioshyn <maksim.lioshyn@itechart-group.com>
2020-02-20 16:06:46 +0300

    Fix varchar to binary in password hash

--- ProjectDB/dbo/Tables/UserPasswords.sql
+++ ProjectDB/dbo/Tables/UserPasswords.sql
@@ -3,7 +3,7 @@
     [Id] UNIQUEIDENTIFIER CONSTRAINT [DF_UserPasswords_Id] DEFAULT (newid()) NOT NULL,
     [UserId] UNIQUEIDENTIFIER NOT NULL, 
     [Salt] BINARY(16) NOT NULL,
-    [Hash] NVARCHAR(32) NOT NULL,
+    [Hash] BINARY(32) NOT NULL,
     CONSTRAINT [PK_UserPasswords] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_UserPasswords_ Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]),
 )
