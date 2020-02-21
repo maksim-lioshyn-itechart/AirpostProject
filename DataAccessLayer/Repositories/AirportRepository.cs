e7a4a0b1a5811066f9d6117c82503f06834b20a0
Maksim Lioshyn <maksim.lioshyn@itechart-group.com>
2020-02-21 17:54:27 +0300

    Create Airport repository and test for it

---
+++ DataAccessLayer/Repositories/AirportRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class AirportRepository : IAirportRepository
+    {
+        private readonly IConfigurationFactory _configuration;
+        public AirportRepository(IConfigurationFactory configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task Create(Airport entity)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            await db.ExecuteAsync("InsertAirport", entity, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<Airport> GetById(Guid id)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            return await db.QuerySingleAsync<Airport>("GetByIdAirport", new { id }, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<IEnumerable<Airport>> GetAll()
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            return await db.QueryAsync<Airport>("GetAllAirport", commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task Update(Airport entity)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            await db.ExecuteAsync("UpdateAirport", entity, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            await db.ExecuteAsync("DeleteAirport", new { id }, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
