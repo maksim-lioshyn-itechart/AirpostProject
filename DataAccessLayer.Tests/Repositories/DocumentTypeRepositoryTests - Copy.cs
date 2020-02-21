00ea43d4c12bdede550bb1dd0117d56d417ccd66
Maksim Lioshyn <maksim.lioshyn@itechart-group.com>
2020-02-21 17:41:21 +0300

    Add DocumentRepository and integration tests

---
+++ DataAccessLayer.Tests/Repositories/DocumentTypeRepositoryTests - Copy.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Dapper;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
+using DataAccessLayer.Repositories;
+using NUnit.Framework;
+
+namespace DataAccessLayer.Tests.Repositories
+{
+    [TestFixture()]
+    public class DocumentRepositoryTests
+    {
+        private readonly IBaseRepository<Document> _repository;
+
+        static readonly DocumentType _entityDocumentType = new DocumentType()
+        {
+            Id = Guid.NewGuid(),
+            Name = Guid.NewGuid().ToString(),
+            
+        };
+
+        readonly Document _entity = new Document()
+        {
+            Id = Guid.NewGuid(),
+            Name = Guid.NewGuid().ToString(),
+            DocumentTypeId = _entityDocumentType.Id,
+            Number = Guid.NewGuid().ToString()
+        };
+
+        public DocumentRepositoryTests()
+        {
+            _repository = new DocumentRepository(new Test());
+        }
+
+        [Test()]
+        [Order(1)]
+        public void CreateTest()
+        {
+            new DocumentTypeRepository(new Test()).Create(_entityDocumentType).Wait();
+            _repository.Create(_entity).Wait();
+        }
+
+        [Test()]
+        [Order(2)]
+        public void GetByIdTest()
+        {
+            var test = _repository.GetById(_entity.Id).Result;
+            Assert.AreEqual(test.Id, _entity.Id);
+        }
+
+        [Test()]
+        [Order(3)]
+        public void GetAllTest()
+        {
+            var tests = _repository.GetAll().Result;
+            Assert.GreaterOrEqual(tests.AsList().Count, 1);
+        }
+
+        [Test()]
+        [Order(4)]
+        public void UpdateTest()
+        {
+            var test = _entity;
+            test.Name = "Test";
+            _repository.Update(test).Wait();
+            var airline = _repository.GetById(_entity.Id).Result;
+            Assert.AreEqual(airline.Name, _entity.Name);
+        }
+
+        [Test()]
+        [Order(100)]
+        public void DeleteTest()
+        {
+            _repository.Delete(_entity.Id).Wait();
+            new DocumentTypeRepository(new Test()).Delete(_entityDocumentType.Id).Wait();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/DocumentRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class DocumentRepository : IDocumentRepository
+    {
+        private readonly IConfigurationFactory _configuration;
+        public DocumentRepository(IConfigurationFactory configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task Create(Document entity)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            await db.ExecuteAsync("InsertDocument", entity, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<Document> GetById(Guid id)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            return await db.QuerySingleAsync<Document>("GetByIdDocument", new { id }, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<IEnumerable<Document>> GetAll()
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            return await db.QueryAsync<Document>("GetAllDocument", commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task Update(Document entity)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            await db.ExecuteAsync("UpdateDocument", entity, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            await db.ExecuteAsync("DeleteDocument", new { id }, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
