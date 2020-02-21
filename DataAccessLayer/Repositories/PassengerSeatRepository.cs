f3a805746992058860eb46a2db9ee53f28b091af
Maksim Lioshyn <maksim.lioshyn@itechart-group.com>
2020-02-21 18:01:45 +0300

    Add PassengerSeatRepository with tests

---
+++ DataAccessLayer/Repositories/PassengerSeatRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class PassengerSeatRepository : IPassengerSeatRepository
+    {
+        private readonly IConfigurationFactory _configuration;
+        public PassengerSeatRepository(IConfigurationFactory configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task Create(PassengerSeat entity)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            await db.ExecuteAsync("InsertPassengerSeat", entity, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<PassengerSeat> GetById(Guid id)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            return await db.QuerySingleAsync<PassengerSeat>("GetByIdPassengerSeat", new { id }, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<IEnumerable<PassengerSeat>> GetAll()
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            return await db.QueryAsync<PassengerSeat>("GetAllPassengerSeat", commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task Update(PassengerSeat entity)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            await db.ExecuteAsync("UpdatePassengerSeat", entity, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            using IDbConnection db = _configuration.GetConnection();
+            await db.ExecuteAsync("DeletePassengerSeat", new { id }, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
